c9eabf38a4f3477f7b4411734eb80e2992522f95
Mike Bowman <mike@mikebowman.dev>
2025-04-26 16:04:28 -0400

    Start using astro components rather than react

--- src/components/IndexContainer.tsx
+++
@@ -1,7 +0,0 @@
-const IndexContainer = ({ children }) => (
-  <div className="m-4 border border-gray-400 p-2 shadow-md lg:mx-0 lg:p-4">
-    {children}
-  </div>
-);
-
-export default IndexContainer;
