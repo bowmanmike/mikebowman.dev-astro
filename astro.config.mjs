ca35149201dbbc0ca366a14b5371e6ed221f29ff
Mike Bowman <mike@mikebowman.dev>
2025-04-26 16:29:38 -0400

    Add icons, progress on index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,19 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    icon({
+      include: {
+        articons: ["*"],
+        logos: ["cib", "github-icon"],
+      },
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
