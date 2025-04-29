133ac3699134a125f854b464af5f117e37dbebdc
Mike Bowman <mike@mikebowman.dev>
2025-04-29 17:49:41 -0400

    Eager load image, run prettier

--- old-site/src/components/Layout.js
+++ old-site/src/components/Layout.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import Header from './Header';
-import Footer from './Footer';
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => (
   <>
     <div className="mx-auto max-w-6xl text-gray-700">
       <Header />
-      <main className="my-4 mx-auto max-w-6xl">{children}</main>
+      <main className="mx-auto my-4 max-w-6xl">{children}</main>
       <Footer />
     </div>
   </>
