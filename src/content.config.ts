f63a64b1a2d8ea5ea639032d10da633bd440a68c
Mike Bowman <mike@mikebowman.dev>
2025-04-30 22:00:20 -0400

    Start rebuilding blog section

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from "astro:content";
+
+// 2. Import loader(s)
+import { glob } from "astro/loaders";
+
+// 3. Define your collection(s)
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/data/blog" }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    slug: z.string(),
+    published: z.boolean(),
+  }),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { blog };
